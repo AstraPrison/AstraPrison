f9a216ad3d200ca9c8c6dabe6068eae3698ffeca
Mantice <nicholas_call@outlook.com>
2024-02-25 04:12:36 -0500

    refactor: reformatted PlaceholderAPI static variable

--- utilities/src/main/java/dev/paracausal/astra/utilities/dependencies/Dependencies.java
+++ utilities/src/main/java/dev/paracausal/astra/utilities/dependencies/Dependencies.java
@@ -1,13 +1,14 @@
 package dev.paracausal.astra.utilities.dependencies;
 
+import me.arcaniax.hdb.api.HeadDatabaseAPI;
 import org.bukkit.Bukkit;
 
 public final class Dependencies {
 
-    public static final boolean hasPlaceholderAPI;
+    public static final boolean HAS_PLACEHOLDER_API;
 
     static {
-        hasPlaceholderAPI = Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null;
+        HAS_PLACEHOLDER_API = Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null;
     }
 
 }
