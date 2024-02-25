806281979ede64bfa93d8b5ad47185153fc1459c
Mantice <nicholas_call@outlook.com>
2024-02-25 04:14:56 -0500

    feat: added HeadDatabase dependency

--- utilities/src/main/java/dev/paracausal/astra/utilities/dependencies/Dependencies.java
+++ utilities/src/main/java/dev/paracausal/astra/utilities/dependencies/Dependencies.java
@@ -6,9 +6,19 @@ import org.bukkit.Bukkit;
 public final class Dependencies {
 
     public static final boolean HAS_PLACEHOLDER_API;
+    public static final boolean HAS_HEAD_DATABASE;
+    public static final HeadDatabaseAPI HEAD_DATABASE_API;
 
     static {
         HAS_PLACEHOLDER_API = Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null;
+        HAS_HEAD_DATABASE = Bukkit.getPluginManager().getPlugin("HeadDatabase") != null;
+
+        if (HAS_HEAD_DATABASE) {
+            HEAD_DATABASE_API = new HeadDatabaseAPI();
+        }
+        else {
+            HEAD_DATABASE_API = null;
+        }
     }
 
 }
