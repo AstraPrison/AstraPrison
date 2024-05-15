0f44d8bd04395a2c25ec9eda8e9637f691056d5c
Mantice <nicholas_call@outlook.com>
2024-05-15 14:18:52 -0400

    feat: added response for main admin command

--- src/main/java/dev/fabled/astra/commands/AstraCommand.java
+++ src/main/java/dev/fabled/astra/commands/AstraCommand.java
@@ -1,6 +1,9 @@
 package dev.fabled.astra.commands;
 
 import com.mojang.brigadier.tree.CommandNode;
+import dev.fabled.astra.Astra;
+import dev.fabled.astra.utils.MiniColor;
+import dev.fabled.astra.utils.logger.AstraLog;
 import net.minecraft.commands.CommandSourceStack;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -23,9 +26,17 @@ public class AstraCommand extends BrigadierCommand {
         return literal(name)
                 .executes(c -> {
                     if (!(getSender(c) instanceof Player player)) {
+                        AstraLog.log(
+                                "AstraPrison v" + Astra.getPlugin().getPluginMeta().getVersion(),
+                                "Developed by Mantice and DrDivx2k",
+                                "Type 'astra help' for more commands!"
+                        );
                         return -1;
                     }
-                    
+
+                    player.sendMessage(MiniColor.parse("AstraPrison v" + Astra.getPlugin().getPluginMeta().getVersion()));
+                    player.sendMessage(MiniColor.parse("Developed by Mantice and DrDivx2k"));
+                    player.sendMessage(MiniColor.parse("<gray>Type <white>/astra help<gray> for more commands!"));
                     return 0;
                 })
                 .build();
