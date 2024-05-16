80c7fed4fc3006e3b6180c3764bc4ac602a82d00
Mantice <nicholas_call@outlook.com>
2024-05-16 10:56:56 -0400

    feat: added methods to get all online player names

--- common/src/main/java/dev/fabled/astra/utils/ListUtils.java
+++ common/src/main/java/dev/fabled/astra/utils/ListUtils.java
@@ -1,12 +1,16 @@
 package dev.fabled.astra.utils;
 
 import dev.fabled.astra.utils.configuration.YamlConfig;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListUtils {
 
@@ -30,6 +34,21 @@ public class ListUtils {
     public static @NotNull List<String> fromConfig(@NotNull final YamlConfig config, @NotNull final String path) {
         final List<String> result = fromConfig(config, path, null);
         return result == null ? new ArrayList<>() : result;
+    }
+
+    public static @NotNull List<String> playerNames() {
+        return Bukkit.getOnlinePlayers().stream().map(Player::getName).collect(Collectors.toList());
+    }
+
+    public static @NotNull List<String> playerNames(@NotNull final CommandSender sender) {
+        final List<String> names = playerNames();
+
+        if (!(sender instanceof Player player)) {
+            return names;
+        }
+
+        names.remove(player.getName());
+        return names;
     }
 
 }
