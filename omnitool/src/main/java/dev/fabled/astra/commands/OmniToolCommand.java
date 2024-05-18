86c5380332502c9517d2981e34ad5276ac755f09
Mantice <nicholas_call@outlook.com>
2024-05-18 11:16:59 -0400

    chore: command structure organization

--- omnitool/src/main/java/dev/fabled/astra/commands/OmniToolCommand.java
+++ omnitool/src/main/java/dev/fabled/astra/commands/OmniToolCommand.java
@@ -1,12 +1,16 @@
 package dev.fabled.astra.commands;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.tree.CommandNode;
+import dev.fabled.astra.lang.LocaleManager;
+import dev.fabled.astra.lang.console.ConsoleLang;
+import dev.fabled.astra.lang.impl.ErrorLang;
+import dev.fabled.astra.lang.impl.OmniToolLang;
 import dev.fabled.astra.omnitool.OmniTool;
-import dev.fabled.astra.utils.ListUtils;
-import dev.fabled.astra.utils.MiniColor;
+import dev.fabled.astra.utils.logger.AstraLog;
+import dev.fabled.astra.utils.logger.AstraLogLevel;
 import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.SharedSuggestionProvider;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -23,50 +27,62 @@ public class OmniToolCommand extends BrigadierCommand {
     }
 
     @Override
-    @NotNull
-    CommandNode<CommandSourceStack> buildCommandNode() {
-        return literal(name)
-                .executes(context -> {
-                    if (!(getSender(context) instanceof Player player)) {
-                        return 0;
-                    }
-
-                    player.sendMessage("OmniTool Admin Commands:");
-                    return 0;
-                })
-
-                .then(literal("give")
-                        .executes(context -> {
-                            if (!(getSender(context) instanceof Player player)) {
-                                return 0;
-                            }
-
-                            player.sendMessage(MiniColor.parse("<red>Select a player!"));
-                            return 0;
-                        })
-
+    @NotNull CommandNode<CommandSourceStack> buildCommandNode() {
+        return literal(name).executes(this::help)
+                .then(literal("give").executes(this::give)
                         .then(arg("player", StringArgumentType.word())
-                                .suggests((context, builder) -> SharedSuggestionProvider.suggest(
-                                        ListUtils.playerNames(), builder
-                                ))
-
-                                .executes(context -> {
-                                    if (!(getSender(context) instanceof Player player)) {
-                                        return 0;
-                                    }
-
-                                    final String playerName = StringArgumentType.getString(context, "player");
-                                    final Player target = Bukkit.getPlayer(playerName);
-                                    if (target == null) {
-                                        player.sendMessage(MiniColor.parse("<red>Invalid player: '<white>" + playerName + "<red>'"));
-                                        return 0;
-                                    }
-
-                                    OmniTool.give(target);
-                                    player.sendMessage(MiniColor.parse("<green>Gave the omni-tool to <white>" + target.getName() + "<green>!"));
-                                    return 0;
-                                })))
+                                .executes(this::givePlayer)
+                                .suggests(this::suggestOnlinePlayers)
+                        )
+                )
                 .build();
+    }
+
+    @SuppressWarnings("SameReturnValue")
+    private int help(@NotNull final CommandContext<CommandSourceStack> context) {
+        if (!(getSender(context) instanceof Player player)) {
+            AstraLog.log(ConsoleLang.OMNI_TOOL_ADMIN_HELP);
+            return 0;
+        }
+
+        LocaleManager.send(player, OmniToolLang.ADMIN_HELP);
+        return 0;
+    }
+
+    @SuppressWarnings("SameReturnValue")
+    private int give(@NotNull final CommandContext<CommandSourceStack> context) {
+        if (!(getSender(context) instanceof Player player)) {
+            AstraLog.log(AstraLogLevel.ERROR, ConsoleLang.SELECT_PLAYER);
+            return 0;
+        }
+
+        LocaleManager.send(player, ErrorLang.SELECT_PLAYER);
+        return 0;
+    }
+
+    private int givePlayer(@NotNull final CommandContext<CommandSourceStack> context) {
+        final String playerName = StringArgumentType.getString(context, "player");
+        final Player target = Bukkit.getPlayer(playerName);
+
+        if (!(getSender(context) instanceof Player player)) {
+            if (target == null) {
+                AstraLog.log(AstraLogLevel.ERROR, ConsoleLang.INVALID_PLAYER + "'" + playerName + "'");
+                return -1;
+            }
+
+            OmniTool.give(target);
+            AstraLog.log(AstraLogLevel.SUCCESS, ConsoleLang.OMNI_TOOL_GIVE + target.getName() + "!");
+            return 0;
+        }
+
+        if (target == null) {
+            LocaleManager.send(player, ErrorLang.INVALID_PLAYER, "{PLAYER}", playerName);
+            return -1;
+        }
+
+        OmniTool.give(target);
+        LocaleManager.send(player, OmniToolLang.ADMIN_GIVE, "{PLAYER}", target.getName());
+        return 0;
     }
 
 }
--- src/main/java/dev/fabled/astra/commands/AstraCommand.java
+++ src/main/java/dev/fabled/astra/commands/AstraCommand.java
@@ -1,13 +1,16 @@
 package dev.fabled.astra.commands;
 
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.tree.CommandNode;
 import dev.fabled.astra.Astra;
+import dev.fabled.astra.lang.LocaleManager;
+import dev.fabled.astra.lang.console.ConsoleLang;
+import dev.fabled.astra.lang.impl.AstraAdminLang;
 import dev.fabled.astra.utils.MiniColor;
 import dev.fabled.astra.utils.logger.AstraLog;
 import net.minecraft.commands.CommandSourceStack;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-
 
 public class AstraCommand extends BrigadierCommand {
 
@@ -23,23 +26,41 @@ public class AstraCommand extends BrigadierCommand {
 
     @Override
     @NotNull CommandNode<CommandSourceStack> buildCommandNode() {
-        return literal(name)
-                .executes(c -> {
-                    if (!(getSender(c) instanceof Player player)) {
-                        AstraLog.log(
-                                "AstraPrison v" + Astra.getPlugin().getPluginMeta().getVersion(),
-                                "Developed by Mantice and DrDivx2k",
-                                "Type 'astra help' for more commands!"
-                        );
-                        return -1;
-                    }
-
-                    player.sendMessage(MiniColor.parse("AstraPrison v" + Astra.getPlugin().getPluginMeta().getVersion()));
-                    player.sendMessage(MiniColor.parse("Developed by Mantice and DrDivx2k"));
-                    player.sendMessage(MiniColor.parse("<gray>Type <white>/astra help<gray> for more commands!"));
-                    return 0;
-                })
+        return literal(name).executes(this::base)
+                .then(literal("help").executes(this::help))
                 .build();
+    }
+
+    @SuppressWarnings({"SameReturnValue", "UnstableApiUsage"})
+    private int base(@NotNull final CommandContext<CommandSourceStack> context) {
+        if (!(getSender(context) instanceof Player player)) {
+            AstraLog.log(
+                    "AstraPrison v" + Astra.getPlugin().getPluginMeta().getVersion(),
+                    "Developed by Mantice and DrDivx2k",
+                    "Type 'astra help' for more commands!"
+            );
+            return 0;
+        }
+
+        player.sendMessage(MiniColor.parse("AstraPrison v" + Astra.getPlugin().getPluginMeta().getVersion()));
+        player.sendMessage(MiniColor.parse("Developed by Mantice and DrDivx2k"));
+        player.sendMessage(MiniColor.parse("<gray>Type <white>/astra help<gray> for more commands!"));
+        return 0;
+    }
+
+    /*
+            TODO (Later On)
+            Replace player help command with a GUI that lists all modules and their commands
+     */
+    @SuppressWarnings("SameReturnValue")
+    private int help(@NotNull final CommandContext<CommandSourceStack> context) {
+        if (!(getSender(context) instanceof Player player)) {
+            AstraLog.log(ConsoleLang.ADMIN_HELP);
+            return 0;
+        }
+
+        LocaleManager.send(player, AstraAdminLang.ADMIN_HELP);
+        return 0;
     }
 
 }
