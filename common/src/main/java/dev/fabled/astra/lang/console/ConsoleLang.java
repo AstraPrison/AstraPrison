5bdfe8ccc38cae1518f909ad4f49d0420d56f699
Mantice <nicholas_call@outlook.com>
2024-05-18 11:14:27 -0400

    feat: static final console messages

---
+++ common/src/main/java/dev/fabled/astra/lang/console/ConsoleLang.java
@@ -0,0 +1,47 @@
+package dev.fabled.astra.lang.console;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+public class ConsoleLang {
+
+    /*
+            Error Messages
+     */
+
+    /** Error Message */
+    public static final String
+            PLAYER_ONLY = "Please run this command as a player!",
+            SELECT_PLAYER = "Select a player!",
+            /** Error Message<br>Append a player's name */
+            INVALID_PLAYER = "Invalid player: ";
+
+    /*
+            Admin Messages
+    */
+
+    /** Admin Message */
+    public static final ImmutableList<String>
+            ADMIN_HELP = ImmutableList.of(
+                    "Astra Commands:",
+                    "| 'astra' - Shows plugin information",
+                    "| 'astra help' - Shows this information",
+                    "| 'astra reload' - Reloads plugin configuration"
+            );
+
+    /*
+            Omni-Tool Messages
+    */
+
+    /** Omni-Tool Message */
+    public static final String
+            OMNI_TOOL_GIVE = "You gave a new omni-tool to ";
+
+    /** Omni-Tool Message */
+    public static final ImmutableList<String>
+            OMNI_TOOL_ADMIN_HELP = ImmutableList.of(
+                    ""
+            );
+
+}
