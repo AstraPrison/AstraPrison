bdc7ed4102b1d6fd7b7169d7649f5cfd1a65bded
Mantice <nicholas_call@outlook.com>
2024-06-26 15:36:14 -0400

    chore: updated command

--- omnitool/src/main/java/dev/fabled/astra/commands/OmniToolCommand.java
+++ omnitool/src/main/java/dev/fabled/astra/commands/OmniToolCommand.java
@@ -1,7 +1,6 @@
 package dev.fabled.astra.commands;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.tree.CommandNode;
 import dev.fabled.astra.lang.LocaleManager;
@@ -32,13 +31,15 @@ public class OmniToolCommand extends BrigadierCommand {
     @Override
     public @NotNull CommandNode<CommandSourceStack> buildCommandNode() {
         return literal(name).executes(this::help)
-                .then(LiteralArgumentBuilder.<CommandSourceStack>literal("giveOmniTool")
+                .then(literal("give")
+                        .requires(permissionRequirement("astra.omnitool.give"))
                         .then(arg("player", StringArgumentType.word())
                                 .suggests(this::suggestOnlinePlayers)
                                 .executes(this::giveOmniTool)))
                 .build();
     }
 
+    @SuppressWarnings("SameReturnValue")
     private int help(@NotNull final CommandContext<CommandSourceStack> context) {
         if (!(getSender(context) instanceof Player player)) {
             AstraLog.log(ConsoleLang.OMNI_TOOL_ADMIN_HELP);
@@ -49,6 +50,7 @@ public class OmniToolCommand extends BrigadierCommand {
         return 0;
     }
 
+    @SuppressWarnings("SameReturnValue")
     private int giveOmniTool(@NotNull final CommandContext<CommandSourceStack> context) {
         final String playerName = StringArgumentType.getString(context, "player");
         final Player target = Bukkit.getPlayer(playerName);
@@ -56,21 +58,23 @@ public class OmniToolCommand extends BrigadierCommand {
         if (target == null) {
             if (!(getSender(context) instanceof Player player)) {
                 AstraLog.log(AstraLogLevel.ERROR, ConsoleLang.INVALID_PLAYER + "'" + playerName + "'");
-                return -1;
+                return 0;
             }
-            LocaleManager.send((Player) getSender(context), ErrorLang.INVALID_PLAYER, "{PLAYER}", playerName);
-            return -1;
+
+            LocaleManager.send(player, ErrorLang.INVALID_PLAYER, "{PLAYER}", playerName);
+            return 0;
         }
 
         ItemStack omniTool = createOmniToolForPlayer(target);
         target.getInventory().addItem(omniTool);
         target.sendMessage("OmniTool given!");
-        resetXPongive(target);
+        resetXPOnGive(target);
         return 0;
     }
 
-    public void resetXPongive(Player player) {
+    private void resetXPOnGive(Player player) {
         player.setExp(0);
         player.setLevel(0);
     }
+
 }
