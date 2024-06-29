1c2de6a1e2f79643aa0cc5bf4bddc793848d8613
Mantice <nicholas_call@outlook.com>
2024-06-29 14:22:38 -0400

    feat: added armor module admin lang

---
+++ armor/src/main/java/dev/fabled/astra/lang/impl/ArmorAdminLang.java
@@ -0,0 +1,14 @@
+package dev.fabled.astra.lang.impl;
+
+import dev.fabled.astra.lang.annotations.LangKey;
+import dev.fabled.astra.lang.interfaces.LangKeys;
+
+public class ArmorAdminLang implements LangKeys {
+
+    @LangKey
+    public static final String
+
+            INVALID_ARMOR = "",
+            INVALID_ARMOR_TYPE = "";
+
+}
