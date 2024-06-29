b276313a137af9f43eb4f6280f575407b61910e2
Mantice <nicholas_call@outlook.com>
2024-06-29 14:20:46 -0400

    feat: class dedicated to explosives module messages

---
+++ explosives/src/main/java/dev/fabled/astra/lang/impl/ExplosivesLang.java
@@ -0,0 +1,20 @@
+package dev.fabled.astra.lang.impl;
+
+import dev.fabled.astra.lang.annotations.LangKey;
+import dev.fabled.astra.lang.interfaces.LangKeys;
+
+public class ExplosivesLang implements LangKeys {
+
+    @LangKey
+    public static final String
+
+            HELP = "explosives.help",
+
+            SELECT_ITEM = "explosives.give.select-item",
+            GIVEN = "explosives.give.given",
+            RECEIVED = "explosives.give.received",
+
+            NO_AMMO = "explosives.no-ammo",
+            INVALID_WORLD = "explosives.invalid-world";
+
+}
