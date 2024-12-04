ca5473404b5fee31f12733577b9b40138bdb9249
Mantice <79672154+DeOpping@users.noreply.github.com>
2024-12-03 20:23:34 -0500

    feat: new and updated permissions

--- common/src/main/java/dev/fabled/astra/permissions/AstraPermission.java
+++ common/src/main/java/dev/fabled/astra/permissions/AstraPermission.java
@@ -22,19 +22,19 @@ public enum AstraPermission {
     ASTRA_STAR(
             "astra.*",
             "Gives all Astra permissions",
-            "astra.admin.*", "astra.mines.*", "astra.omnitool.*"
+            "astra.admin.*", "astra.mines.*", "astra.omnitool.*", "astra.sellprice.*"
     ),
 
     ADMIN_STAR(
             "astra.admin.*",
             "Gives all Astra admin permissions",
-            "astra.admin", "astra.admin.help", "astra.admin.reload", "astra.admin.menu.other"
+            "astra.admin.help", "astra.admin.reload", "astra.admin.menu.other"
     ),
 
     ADMIN("astra.admin", "Permission for /astra"),
-    ADMIN_HELP("astra.admin.help", "Permission for /astra help"),
-    ADMIN_RELOAD("astra.admin.reload", "Permission for /astra reload"),
-    ADMIN_MENU("astra.admin.menu", "Permission for /astra menu"),
+    ADMIN_HELP("astra.admin.help", "Permission for /astra help", "astra.admin"),
+    ADMIN_RELOAD("astra.admin.reload", "Permission for /astra reload", "astra.admin"),
+    ADMIN_MENU("astra.admin.menu", "Permission for /astra menu", "astra.admin"),
     ADMIN_MENU_OTHER(
             "astra.admin.menu.other",
             "Permission for /astra menu <player>",
@@ -44,10 +44,10 @@ public enum AstraPermission {
     OMNI_TOOL_STAR(
             "astra.omnitool.*",
             "Gives all omnitool permissions",
-            "astra.omnitool", "astra.omnitool.give"
+            "astra.omnitool.give"
     ),
     OMNI_TOOL("astra.omnitool", "Permission for /omnitool"),
-    OMNI_TOOL_GIVE("astra.omnitool.give", "Permission for /omnitool give"),
+    OMNI_TOOL_GIVE("astra.omnitool.give", "Permission for /omnitool give", "astra.omnitool"),
 
     MINES_STAR(
             "astra.mines.*",
@@ -58,11 +58,20 @@ public enum AstraPermission {
     MINE_WAND_STAR(
             "astra.mines.wand.*",
             "Gives all mine wand permissions!",
-            "astra.mines.wand", "astra.mines.wand.give", "astra.mines.wand.use"
+            "astra.mines.wand.other", "astra.mines.wand.use"
     ),
     MINE_WAND("astra.mines.wand", "Permission for /minewand"),
-    MINE_WAND_OTHER("astra.mines.wand.other", "Permission for /minewand <player>"),
-    MINE_WAND_USE("astra.mine.wand.use", "Permission to use the mine wand");
+    MINE_WAND_OTHER("astra.mines.wand.other", "Permission for /minewand <player>", "astra.mines.wand"),
+    MINE_WAND_USE("astra.mine.wand.use", "Permission to use the mine wand"),
+
+    SELL_PRICE_STAR(
+            "astra.sellprice.*",
+            "Gives all sell price permissions!",
+            "astra.sellprice.set", "astra.sellprice.check"
+    ),
+    SELL_PRICE("astra.sellprice", "Permission for /sellprice"),
+    SELL_PRICE_SET("astra.sellprice.set", "Permission for /sellprice set", "astra.sellprice"),
+    SELL_PRICE_CHECK("astra.sellprice.check", "Permission for /sellprice check", "astra.sellprice");
 
     private final @NotNull String name;
     private final @NotNull String description;
