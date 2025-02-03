ff9439b70f7b64ef9854dd846d877f03050ac215
Mantice <79672154+DeOpping@users.noreply.github.com>
2025-02-03 17:56:05 -0500

    feat: menu item namespaced key

--- common/src/main/java/dev/fabled/astra/Astra.java
+++ common/src/main/java/dev/fabled/astra/Astra.java
@@ -1,14 +1,17 @@
 package dev.fabled.astra;
 
+import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 public final class Astra {
 
     private static JavaPlugin plugin;
+    private static NamespacedKey astraMenuItemKey;
 
     static void onLoad(final @NotNull JavaPlugin plugin) {
         Astra.plugin = plugin;
+        astraMenuItemKey = new NamespacedKey(plugin, "astra-menu-item");
     }
 
     public static AstraPlugin get() {
@@ -17,6 +20,10 @@ public final class Astra {
 
     public static JavaPlugin getPlugin() {
         return plugin;
+    }
+
+    public static NamespacedKey getAstraMenuItemKey() {
+        return astraMenuItemKey;
     }
 
 }
