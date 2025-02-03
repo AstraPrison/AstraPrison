e899898b0267864c6561ecfa790bfde3f3edf98a
Mantice <79672154+DeOpping@users.noreply.github.com>
2025-02-03 18:05:30 -0500

    feat: ID for menu items

--- common/src/main/java/dev/fabled/astra/menus/MenuItem.java
+++ common/src/main/java/dev/fabled/astra/menus/MenuItem.java
@@ -1,8 +1,10 @@
 package dev.fabled.astra.menus;
 
+import dev.fabled.astra.Astra;
 import dev.fabled.astra.utils.SlotUtils;
 import dev.fabled.astra.utils.configuration.YamlConfig;
 import dev.fabled.astra.utils.items.ItemBuilder;
+import dev.fabled.astra.utils.items.pdc.StringPDC;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -11,19 +13,28 @@ import java.util.Set;
 
 public class MenuItem extends ItemBuilder {
 
+    private final @NotNull String id;
     private @Nullable Set<Integer> slots;
 
-    public MenuItem(@NotNull String material) {
+    public MenuItem(final @NotNull String id, final @NotNull String material) {
         super(material);
+        this.id = id;
     }
 
-    public MenuItem(final @NotNull YamlConfig config, @NotNull String key) {
+    public MenuItem(final @NotNull YamlConfig config, @NotNull String key, final @NotNull String id) {
         super(config, key);
+        this.id = id;
         if (!key.endsWith(".")) {
             key += ".";
         }
 
         slots = SlotUtils.fromConfig(config, key + "slots");
+
+        addPDC(new StringPDC(Astra.getAstraMenuItemKey(), id));
+    }
+
+    public final @NotNull String getId() {
+        return id;
     }
 
     public @NotNull MenuItem setSlots(final @Nullable Set<Integer> slots) {
