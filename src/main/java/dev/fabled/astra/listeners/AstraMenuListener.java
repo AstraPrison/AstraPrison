3c8e355ebe424f563a6cf72c0e7ade6f2963780b
Mantice <79672154+DeOpping@users.noreply.github.com>
2025-02-03 18:08:40 -0500

    feat: click actions and requirements functionality

--- src/main/java/dev/fabled/astra/listeners/AstraMenuListener.java
+++ src/main/java/dev/fabled/astra/listeners/AstraMenuListener.java
@@ -1,11 +1,18 @@
 package dev.fabled.astra.listeners;
 
+import dev.fabled.astra.Astra;
 import dev.fabled.astra.menus.AstraMenu;
+import dev.fabled.astra.menus.MenuClickType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.Set;
 
@@ -18,18 +25,48 @@ public class AstraMenuListener implements AstraListener {
         }
 
         final Inventory topInventory = player.getOpenInventory().getTopInventory();
-        if (!(topInventory.getHolder() instanceof AstraMenu)) {
+        if (!(topInventory.getHolder() instanceof AstraMenu astraMenu)) {
             return;
         }
 
         final int size = topInventory.getSize() - 1;
         final int slot = event.getRawSlot();
-
         if (slot < 0 || slot > size) {
             return;
         }
 
         event.setCancelled(true);
+
+        final ItemStack clickedItem = event.getCurrentItem();
+        if (clickedItem == null) {
+            return;
+        }
+
+        final ItemMeta meta = clickedItem.getItemMeta();
+        if (meta == null) {
+            return;
+        }
+
+        final PersistentDataContainer container = meta.getPersistentDataContainer();
+        final String itemId = container.get(Astra.getAstraMenuItemKey(), PersistentDataType.STRING);
+        if (itemId == null) {
+            return;
+        }
+
+        if (!astraMenu.checkRequirements(player, itemId, MenuClickType.ANY)) {
+            return;
+        }
+
+        astraMenu.runClickActions(player, itemId, MenuClickType.ANY);
+
+        final ClickType clickType = event.getClick();
+        final MenuClickType menuClickType = MenuClickType.fromClickType(clickType);
+
+        if (!astraMenu.checkRequirements(player, itemId, menuClickType)) {
+            return;
+        }
+
+        astraMenu.runClickActions(player, itemId, menuClickType);
     }
 
     @EventHandler
